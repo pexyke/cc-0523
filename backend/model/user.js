4e931496c87a48b625ac05bb50430059f397e450
pexyke <pekopeter0@gmail.com>
2022-06-07 11:41:48 +0200

    github oauth flow added

--- backend/model/user.js
+++ backend/model/user.js
@@ -13,10 +13,9 @@ const dashboardSchema = new mongoose.Schema({
 
 const userSchema = new mongoose.Schema({
   username: { type: String }, // empty string NONO!
-  // email: { type: String, unique: true, required: true }, // empty string NONO! + validation
   providers: {
-    google: { type: String, required: false, unique: true },
-    facebook: { type: String, required: false, unique: true },
+    google: { type: String },
+    github: { type: String },
   },
   dashboards: [dashboardSchema], // empty list is default?
 });
--- backend/util/http.js
+++ backend/util/http.js
@@ -6,9 +6,9 @@ const http = (baseUrl) => {
     timeout: 3000,
   });
 
-  const post = async (url, body) => {
+  const post = async (...params) => {
     try {
-      const response = await instance.post(url, body);
+      const response = await instance.post(...params);
       return response;
     } catch (err) {
       console.log(err);
@@ -19,3 +19,26 @@ const http = (baseUrl) => {
 };
 
 module.exports = http;
+
+/*
+const { default: axios } = require("axios");
+
+const http = (baseurl) => {
+  const instance = axios.create({
+    baseURL: baseurl || "",
+    timeout: 3000,
+  });
+  const post = async (...params) => {
+    try {
+      const response = await instance.post(...params);
+      console.log("BODY:", response.data);
+      return response;
+    } catch (err) {
+      return err.response;
+    }
+  };
+  return { post };
+};
+
+module.exports = http;
+*/
