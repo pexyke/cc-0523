752e61b588edc9e7ec6a894561f9e690ee7f7678
pexyke <pekopeter0@gmail.com>
2022-06-14 13:59:37 +0200

    render bug fixed

--- frontend/src/hooks/useCounter.js
+++ frontend/src/hooks/useCounter.js
@@ -1,7 +1,9 @@
 import { React, useEffect, useState } from "react";
 
 export const useCounter = (componentName) => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(
+    Number(localStorage.getItem(`counter${componentName}`))
+  );
 
   const increment = () => {
     setCounter(counter + 1);
@@ -12,15 +14,8 @@ export const useCounter = (componentName) => {
   };
 
   useEffect(() => {
-    localStorage.setItem("counter" + componentName, counter);
-    // eslint-disable-next-line
-  }, [counter]);
-
-  useEffect(() => {
-    const localCounter = parseInt(localStorage.getItem("counter" + componentName));
-    setCounter(localCounter || 0);
-    // eslint-disable-next-line
-  }, []);
+    localStorage.setItem(`counter${componentName}`, counter);
+  }, [counter, componentName]);
 
   return { counter, increment, decrement };
 };
